24bce5ee8d478acb7f27f95e93204e17a0cc9e79
venom01k <135177210+venom01k@users.noreply.github.com>
2023-08-07 00:54:27 +0300

    Update checktele.py

--- checktele.py
+++ checktele.py
@@ -191,9 +191,9 @@ def gen_user(choice):
         else:
             pass
             if choice == "14":
-               c = d = random.choices(a)
-               d = random.choices(b)
-               f = [c[0], d[0], "_", c[0], c[0]]
+                c = d = random.choices(a)
+                d = random.choices(b)
+                f = [c[0], d[0], "_", c[0], c[0]]
         random.shuffle(f)
         username = ''.join(f)
         if username in banned[0]:
@@ -205,10 +205,10 @@ def gen_user(choice):
         else:
             pass
             if choice == "12":
-               c = random.choices(a)
-               d = random.choices(b)
-               s = random.choices(e)
-               f = [c[0], s[0], d[0]]
+                c = random.choices(a)
+                d = random.choices(b)
+                s = random.choices(e)
+                f = [c[0], s[0], d[0]]
         random.shuffle(f)
         username = ''.join(f)
         username = username+'bot'
@@ -223,10 +223,10 @@ def gen_user(choice):
         else:
             pass
             if choice == "11":
-               c = random.choices(a)
-               d = random.choices(b)
-               s = random.choices(e)
-               f = [c[0], s[0]]
+                c = random.choices(a)
+                d = random.choices(b)
+                s = random.choices(e)
+                f = [c[0], s[0]]
         random.shuffle(f)
         username = ''.join(f)
         username = username+'bot'
@@ -255,10 +255,10 @@ def gen_user(choice):
         else:
             pass
             if choice == "15":
-               c = random.choices(a)
-               d = random.choices(b)
-               s = random.choices(e)
-               f = [c[0], c[0], "_", d[0]]
+                c = random.choices(a)
+                d = random.choices(b)
+                s = random.choices(e)
+                f = [c[0], c[0], "_", d[0]]
         random.shuffle(f)
         username = ''.join(f)
         username = username+'bot'
